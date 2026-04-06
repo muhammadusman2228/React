618cfdff3f4a84d979671d88cffc54c06c620015
muhammadusman2228 <usmanbhatti504021@gmail.com>
2026-04-06 19:07:43 +0500

    Inializing new react app

---
+++ First Project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App=()=>{
+  return
+  <>
+  <div className="Container">
+    <h1>My First React Applicaton and i am here to start it</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque totam deserunt possimus asperiores reiciendis aperiam, est minima quaerat minus adipisci, iusto beatae dicta odit quas ad molestias praesentium et hic veritatis nemo. Doloribus, eius. Sed ipsa odio beatae corrupti molestias laboriosam impedit unde dolor ex!</p>
+  </div>
+  <div>
+    <h2>I am again here now let's do some work and examine how actually the things will happens </h2>
+  </div>
+  
+  </>
+}
+export default App

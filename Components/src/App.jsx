71f3fd2d5566ae2c2a010c4331266c63a48c2e1d
muhammadusman2228 <usmanbhatti504021@gmail.com>
2026-04-07 09:16:06 +0500

    learning components concept in react

---
+++ Components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css'
+import Navbar from "./components/navbar";
+const App=()=>{
+  return(
+    <>
+<Navbar/>
+<Navbar/>
+<Navbar/>
+    <div className="div">
+      <h1>My Name is Usman</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ut a mollitia nisi sequi tenetur voluptates nulla aliquam neque possimus.</p>
+    </div>
+    </>
+  )
+}
+export default App
---
+++ Components/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css"
+import "./navbar.css"
+const Navbar=()=>{
+    return <div className="navbar">
+        <ul>
+            <li>Dummy Content</li>
+            <li>Dummy Content</li>
+            <li>Dummy Content</li>
+            <li>Dummy Content</li>
+        </ul>
+    </div>
+}
+export default Navbar
